4b1d3d79d46539647575b30a0d4500581c741425
Marcus Vínicius <marcussvgb@hotmail.com>
2020-10-13 17:45:04 -0300

    Finalizando o segundo dia

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,6 +1,27 @@
-const map = L.map("mapid").setView([51.505, -0.09], 13);
+// create map
+const map = L.map("mapid").setView([-10.9360857, -37.0717485], 15);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+// create and add TileLayer
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+
+// create popup overlay
+const popup = L.popup({
+  closebutton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minheight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
+
+// create and add marker
+L.marker([-10.9360857, -37.0717485], { icon })
+.addTo(map)
+.bindPopup(popup)
