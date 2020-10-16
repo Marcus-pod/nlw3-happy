c0e75a96dd72eb3934df598ea886155180266088
Marcus Vinícius <marcussvgb@hotmail.com>
2020-10-16 18:47:38 -0300

    Finalizando o quarto dia de NLW

---
+++ src/database/fakedata.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-10.9360857",
+        lng: "-37.0717485",
+        name: "Lar das meninas",
+        description: "aoeiuaioeuaoieuaieouaioeuaioeuaie",
+        images: ["https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+    "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4"],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas Das 18h até 18h",
+        open_on_weekends: "1"
+    }
+    {
+        id: 2,
+        lat: "-10.9360857",
+        lng: "-37.0817485",
+        name: "Lar dos meninos",
+        description: "aoeiuaioeuaoieuaieouaioeuaioeuaie",
+        images: ["https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+    "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4"],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas Das 18h até 18h",
+        open_on_weekends: "0"
+    }
+]
